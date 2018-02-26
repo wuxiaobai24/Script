6bff0e3c93973bb4946777b8db31e7a8d962fc30
wuxiaobai24 <wuxiaobai24@foxmail.com>
2018-02-26 22:49:36 +0800

    实现Dr.com登录脚本

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # Script
 some useful Script
+
+## Dr.com Logib Script
+
+>  Dr.com 登录脚本（适用于SZU）
+
+- 只在Linux环境中测试过。
+
+- 由于编码问题只适用于python3
+
+- 使用方法：
+
+  ```shell
+  usage: drcom.py [-h] [--daemon] --username USERNAME --passwd PASSWORD
+                  [--gap GAP]
+
+  a script for login dr.com in SZU
+
+  optional arguments:
+    -h, --help            show this help message and exit
+    --daemon, -d          whether become daemon
+    --username USERNAME, -u USERNAME
+                          username
+    --passwd PASSWORD, -p PASSWORD
+                          password
+    --gap GAP, -g GAP     check gap(second)
+  ```
+
+  注意事项：
+
+  1. `gap <= 0`时，表示不循环执行，只进行一次登录，例如`./drcom.py -u USERNAME -p PASSWD -g 0`
+  2. `-d` 选项设置后，会使得程序变成守护进程，这时会生成一个`kill_daemon.sh` 脚本来kill掉守护进程。
+
+  ​
+
+  ​
+
---
+++ drcom.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 26 19:50:29 2018
+
+@author: wuxiaobai24
+"""
+
+import requests
+import sched
+import time
+import os
+import sys
+import argparse
+
+schedule = sched.scheduler(time.time, time.sleep)
+
+username=""
+passwd = ""
+
+parser = argparse.ArgumentParser(description='a script for login dr.com in SZU')
+parser.add_argument('--daemon', '-d',
+                    dest='daemon',
+                    action='store_true', 
+                    help='whether become daemon')
+parser.add_argument('--username', '-u', type=str, required=True,
+                    dest='username',
+                    help = 'username')
+parser.add_argument('--passwd', '-p', type=str,
+                    dest='password', required=True,
+                    help = 'password')
+
+parser.add_argument('--gap', '-g', type=int,
+                    dest='gap', default=5 * 60,
+                    help = 'check gap(second)')
+
+
+def tryLogin(inc):
+    while isLogin():
+        login()
+    schedule.enter(inc, 0, tryLogin, (inc, ))
+
+
+def daemon():
+    '''become daemon'''
+    print('become daemon')
+    print('PID is %d' % os.getpid())
+    file_path = os.path.abspath('.') + '/kill_daemon.sh'
+    print('kill shell in',file_path)
+    
+    pid = os.fork()
+    if pid:
+        # father process exit
+        sys.exit(0)
+    os.chdir('/')
+    os.umask(0)
+    os.setsid()
+    sys.stderr.flush()
+    sys.stdout.flush()
+
+    with open('/dev/null') as read_null, open('/dev/null', 'w') as write_null:
+        os.dup2(read_null.fileno(), sys.stdin.fileno())
+        os.dup2(write_null.fileno(), sys.stderr.fileno())
+        os.dup2(write_null.fileno(), sys.stdout.fileno())
+
+    # save the kill shell
+
+    s = "#!/bin/bash\nkill %d\nrm %s" % (os.getpid(), file_path)
+    with open(file_path, 'w') as f:
+        f.write(s)
+    
+    os.system('chmod +x %s' % file_path)
+
+def login():
+    url = 'http://172.30.255.2/a30.htm'
+    headers = {}
+    headers['Accept'] = 'ext/html,application/xhtml+xml,application/xml' \
+    ';q=0.9,*/*;q=0.8r'
+    headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64; rv:58.0) ' \
+    'Gecko/20100101 Firefox/58.0'
+    headers['Content-Type'] = 'application/x-www-form-urlencoded'
+    headers['Content-Length'] = '51'
+
+    data = {'0MKKey': '登　录'.encode('gb2312'),
+            'DDDDD': username,
+            'upass': passwd}
+    r = requests.post(url, data=data, headers=headers)
+    r.encoding = 'gb2312'
+    if '您已经成功登录。' in r.text:
+        print('Success')
+        return True
+    else:
+        print('Fail')
+        return False
+
+
+def isLogin():
+    try:
+        r = requests.get('http://www.baidu.com/', timeout=10)
+        if r.status_code != requests.status_codes.codes.ok:
+            return False
+        else:
+            return True
+    except Exception as e:
+        return False
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    if args.daemon:
+        daemon()
+    username = args.username
+    passwd = args.password
+    gap = args.gap
+    schedule.run()
+    if gap <= 0:
+        login()
+    else:
+        tryLogin(gap)
